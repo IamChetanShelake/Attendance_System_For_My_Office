b3449e13e74b1a0ee8df7459ebb4887b8cc0e73a
unknown <chetanshelake147@gmail.com>
2025-11-13 10:20:21 +0530

    first commit from raj

---
+++ storage/framework/views/dcffc3f9263c2651bb9afc1f4e22ac20.php
@@ -0,0 +1,6 @@
+<?php echo $__env->make('admin.layout.header', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+
+<?php echo $__env->yieldContent('content'); ?>
+
+<?php echo $__env->make('admin.layout.footer', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
+<?php /**PATH C:\Users\LENOVO\Desktop\Tms_attendance_system\tms_attendance\resources\views/admin/layout/master.blade.php ENDPATH**/ ?>
